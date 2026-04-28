63b25c157080c9d0c88ee9acbfc6de2167de8d9d
Kyrene Li <li003631@umn.edu>
2026-04-27 22:04:40 -0500

    create ml_text_tbl

--- R/final.R
+++ R/final.R
@@ -12,6 +12,20 @@ library(ranger)
 library(xgboost)
 library(httr)
 library(jsonlite)
+library(parallel)
+library(doParallel)
 set.seed(1234)
 
+glassdoor_raw_tbl <- read_csv("../data/glassdoor_reviews.csv")
+
+ml_text_tbl <- glassdoor_raw_tbl %>%
+  select(overall_rating, headline, pros, cons) %>%
+  mutate(review_id = row_number(),
+         review_text = str_squish(paste(replace_na(headline, ""),
+                                        replace_na(pros, ""),
+                                        replace_na(cons, ""),
+                                   sep = " ")
+                                  )
+         )
+ml_text_tbl$review_text
 
